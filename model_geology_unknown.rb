63d19ffd0e3b63223604840a0be78d3b96702a3e
Luke Sparreo <lsparreo@nybg.org>
2025-03-24 11:21:44 -0400

    Update model_geology_unknown.rb

--- model_geology_unknown.rb
+++ model_geology_unknown.rb
@@ -3,7 +3,7 @@
 range_fn = "simulated_range.nex"
 mol_fn = "modified_sequences.nex"
 tree_fn = "collapsed_newick.tre"
-out_fn = "output_geo_unknown_2/simulationoutput" #MODIFY EACH RUN!
+out_fn = "output_geo_unknown_1/simulationoutput" #MODIFY EACH RUN!
 geo_fn = "/Users/lukesparreo/simulated_data/unknown"
 times_fn = geo_fn + ".times.txt" #MODIFY EACH RUN!
 dist_fn = geo_fn + ".distances.txt"
