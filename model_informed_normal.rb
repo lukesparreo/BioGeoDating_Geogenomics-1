458581312458309aefa16ea682ffb1daf917acc2
Luke Sparreo <lsparreo@nybg.org>
2025-04-02 13:01:51 -0400

    Update model_informed_normal.rb

--- model_informed_normal.rb
+++ model_informed_normal.rb
@@ -1,6 +1,6 @@
 ### REVBAYES CODE FOR GEO-INFORMED NORMAL MODEL ###
 range_fn = "simulated_range.nex"
-mol_fn = "modified_sequences.nex"
+mol_fn = "modified_sequences_filled.nex"
 tree_fn = "collapsed_newick.tre"
 out_fn = "output_informed_normal_2/simulationoutput" #MODIFY EACH RUN!
 geo_fn = "/Users/lukesparreo/simulated_data/simulated"
