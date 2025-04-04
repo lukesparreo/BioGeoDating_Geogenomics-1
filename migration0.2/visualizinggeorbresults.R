856f14fe11ff31fdc1a9232b7e1fa0de8afdf0f9
ambed0ya <abedoya@nybg.org>
2025-04-04 08:28:03 -0400

    .

---
+++ migration0.2/visualizinggeorbresults.R
@@ -0,0 +1,16 @@
+library(RevGadgets)
+library(coda)
+library(ggplot2)
+library(ggtree)
+library(grid)
+library(gridExtra)
+
+setwd("~/simulated_data/output_aware_uniform_relaxed_2/")
+file<-"simulationoutput.model.log"
+
+
+trace_quant <- readTrace(path = file, burnin = 0.1)
+
+summarizeTrace(trace = trace_quant, vars =  c("epoch_times[1]","epoch_times[2]"))
+
+plotTrace(trace = trace_quant, vars = c("epoch_times[1]","epoch_times[2]"))[[1]]
