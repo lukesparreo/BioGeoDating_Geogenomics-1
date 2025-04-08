7b0a4e582d8be54a051ac39efc8b57376f160c7b
Luke Sparreo <lsparreo@nybg.org>
2025-04-08 13:31:31 -0400

    Update model_incorrect_normal.rb

--- migration0/scripts/model_incorrect_normal.rb
+++ migration0/scripts/model_incorrect_normal.rb
@@ -2,7 +2,7 @@
 range_fn = "simulated_range.nex"
 mol_fn = "modified_sequences_filled.nex"
 tree_fn = "collapsed_newick.tre"
-out_fn = "output_incorrect_normal_2/simulationoutput" #MODIFY EACH RUN!
+out_fn = "output_incorrect_normal1" #MODIFY EACH RUN!
 geo_fn = "/Users/lukesparreo/simulated_data/simulated"
 times_fn = geo_fn + ".times.incorrect.txt" #MODIFY EACH RUN!
 dist_fn = geo_fn + ".distances.txt"
